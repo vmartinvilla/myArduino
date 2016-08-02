48301726546f0e39b4373e78bf371f32771f697d
Martin Villasenor <vmartinvilla@gmail.com>
2016-08-02 15:07:55 -0700

    Update arm.c

--- arm.c
+++ arm.c
@@ -1,8 +1,3 @@
-
-/* Robot Arm project with 4 servos  */
-//Author: Martin Villasenor
-// Date: 8/2/16
-
 #include <Servo.h>
 
 
@@ -18,6 +13,12 @@ const int arm_servo2 = 10;
 const int arm_servo3 = 5;
 const int arm_servo4 = 11;
 
+/*  arm_servo1 moves base 180 degrees
+ *  arm_servo2 moves in parallel with arm_servo3 on the right side
+ *  arm_servo3 moves in parallel with arm_servo2 on the right side
+ *  arm_servo4 move the claw for grip
+ *  
+ */
 
 //   Angle positions for each servo
 int pos1 = 0;   // For servo position 1
@@ -36,20 +37,18 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-    for ( pos1=0; pos1 <= 180; pos1 +=1) {  // loop 180 degrees
+// Start moving base by 90 degrees
+    for ( pos1=0; pos1 <= 90; pos1 +=1) {  // loop 90 degrees
       myservo1.write(pos1);  // apply motion by degree
       delay(15);  //delay 15ms for each degree
       }
-    
-    for ( pos1 = 180; pos1 >= 0; pos1 -= 1 ) {
 
-      myservo1.write(pos1);
-      delay(15);
-
+    // return to initial position.  Note: this is only for initial testing
+ 
+    for ( pos1=90; pos1 >= 0; pos1 -=1) {  // loop 90 degrees
+      myservo1.write(pos1);  // apply motion by degree
+      delay(15);  //delay 15ms for each degree
+      }
       
-    }
-
-
 }
